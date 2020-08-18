aa6565075dd7db0dde4807e7bcd60a58777faf75
Diana <dianairis1610@gmail.com>
2020-08-18 19:00:40 +0200

    cambios

--- src/components/Shops/Shops.js
+++ src/components/Shops/Shops.js
@@ -1,16 +1,21 @@
 import React from 'react'
 import Shop from './Shop/Shop'
 import chickens from './icons/chickens.svg';
+import fish from './icons/fish.svg';
+import cheese from './icons/cheese.svg';
+import meat from './icons/meat.svg';
+import wine from './icons/wine.svg';
+
 
 
 export default function Shops() {
     return (
         <>
-        <Shop name="Tienda1" icon={chickens}/>
-        <Shop name="Tienda2" icon={chickens}/>
-        <Shop name="Tienda3" icon={chickens}/>
-        <Shop name="Tienda4" icon={chickens}/>
-        <Shop name="Tienda5" icon={chickens}/>
+        <Shop name="TIENDA 1" icon={chickens}/>
+        <Shop name="TIENDA 2" icon={fish}/>
+        <Shop name="TIENDA 3" icon={cheese}/>
+        <Shop name="TIENDA 4" icon={meat}/>
+        <Shop name="TIENDA 5" icon={wine}/>
         </>
     )
 }
